4e2d1f9a1512ca7c2d3218e7574141ffa0c4c722
taolei <taolei@vk.com>
2025-10-25 12:15:37 +0800

    Update

---
+++ blog/.vitepress/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <img class="not-found__image" :src="svgSrc" alt="404 Not Found" />
+    <p class="not-found__text">抱歉，未找到该页面</p>
+    <a class="not-found__link" href="/">返回首页</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withBase } from 'vitepress'
+const svgSrc = withBase('/not-found.svg')
+</script>
+
+<style scoped>
+
+</style>
